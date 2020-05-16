fd5612a1a8db1af372e57430dbe89e9893db2d6e
zhaohq <610006480@qq.com>
2020-05-16 23:19:33 +0800

    init

---
+++ src/core/render/compiler/link.js
@@ -0,0 +1,62 @@
+import { getAndRemoveConfig } from '../utils';
+import { isAbsolutePath } from '../../router/util';
+
+export const linkCompiler = ({ renderer, router, linkTarget, compilerClass }) =>
+  (renderer.link = (href, title = '', text) => {
+    let attrs = [];
+    const { str, config } = getAndRemoveConfig(title);
+
+    title = str;
+
+    if (
+      !isAbsolutePath(href) &&
+      !compilerClass._matchNotCompileLink(href) &&
+      !config.ignore
+    ) {
+      if (href === compilerClass.config.homepage) {
+        href = 'README';
+      }
+
+      href = router.toURL(href, null, router.getCurrentPath());
+    } else {
+      if (!isAbsolutePath(href) && href.startsWith('./')) {
+        href =
+          document.URL.replace(/\/(?!.*\/).*/, '/').replace('#/./', '') + href;
+      }
+      attrs.push(href.indexOf('mailto:') === 0 ? '' : `target="${linkTarget}"`);
+    }
+
+    if (config.target) {
+      attrs.push(`target="${config.target}"`);
+    }
+
+    // special case to check crossorigin urls
+    if (
+      config.crossorgin &&
+      linkTarget === '_self' &&
+      compilerClass.config.routerMode === 'history'
+    ) {
+      if (compilerClass.config.crossOriginLinks.indexOf(href) === -1) {
+        compilerClass.config.crossOriginLinks.push(href);
+      }
+    }
+
+    if (config.disabled) {
+      attrs.push('disabled');
+      href = 'javascript:void(0)';
+    }
+
+    if (config.class) {
+      attrs.push(`class="${config.class}"`);
+    }
+
+    if (config.id) {
+      attrs.push(`id="${config.id}"`);
+    }
+
+    if (title) {
+      attrs.push(`title="${title}"`);
+    }
+
+    return `<a href="${href}" ${attrs.join(' ')}>${text}</a>`;
+  });
